ee4c2ca837d9e1419483c7a479d1e0c1d1d640cc
Elizabethz7 <7721Elizabethz7@users.noreply.github.com>
2020-08-21 23:36:58 -0600

    Navegador
    Navegador

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PortadaComponent } from './components/portada/portada.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PortadaComponent
   ],
   imports: [
     BrowserModule,
