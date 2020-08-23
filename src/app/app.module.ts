367ea3f8aac6b15e0ec83e61143887d8b0535be0
Fernando Chajon <3218285340505@ingenieria.usac.edu.gt>
2020-08-22 23:26:43 -0600

    se agrego el modulo httpClietModule y FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms'
+import {HttpClientModule} from '@angular/common/http';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -17,7 +19,9 @@ import { CicloEscolarComponent } from './components/ciclo-escolar/ciclo-escolar.
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [
     CicloEscolarService
