0abb69a409d89c42b01beb3d8bf58b69be7bf65c
Fernando Chajon <3218285340505@ingenieria.usac.edu.gt>
2020-08-22 23:28:43 -0600

    Se creo un tabla que contiene la informacion de ciclos escolares, con modales para actulizar y eliminar pero sin funcionalidad

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import {CicloEscolarComponent} from './components/ciclo-escolar/ciclo-escolar.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/',
+    pathMatch: 'full'
+  },
+
+  {
+    path: 'ciclos',
+    component: CicloEscolarComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/ciclo-escolar/ciclo-escolar.component.ts
+++ src/app/components/ciclo-escolar/ciclo-escolar.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import {CicloEscolarService} from '../../services/ciclo_escolar/ciclo-escolar.service'
+import {Ciclo} from '../../models/ciclo-escolar.interface';
+
 @Component({
   selector: 'app-ciclo-escolar',
   templateUrl: './ciclo-escolar.component.html',
@@ -9,7 +12,21 @@ export class CicloEscolarComponent implements OnInit {
 
   constructor() { }
 
+  listaCiclo: Array<Ciclo> = [
+    {Ciclo: 1, year:2010},
+    {Ciclo: 2, year:2011},
+    {Ciclo: 3, year:2012},
+    {Ciclo: 4, year:2013}
+  ];
+
+  cicloOperacional: Ciclo;
+
   ngOnInit(): void {
+  }
+
+
+  Obtener_Ciclo(item: Ciclo): void{
+    this.cicloOperacional = item;
   }
 
 }
