fa37a0aca572e7af7a92881ae07141df1ec21f9b
gerchay <2995058550101@ingenieria.usac.edu.gt>
2020-08-23 19:34:35 -0600

    CRUD materias

--- src/app/services/materias/materia.service.ts
+++ src/app/services/materias/materia.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Materia } from '../../models/materia'
 
 @Injectable({
   providedIn: 'root'
 })
 export class MateriaService {
 
-  constructor() { }
+  //Variable para la dirección IP del host
+  API_URI = 'direccion';
+
+  constructor(private http: HttpClient) { }
+
+  getMaterias = () =>  this.http.get(`${this.API_URI}/materias`);
+  getCiclo = (id: string) => this.http.get(`${this.API_URI}/materias/${id}`);
+  saveCiclo = (materia: Materia) => this.http.post(`${this.API_URI}/materias`,materia);
+  deleteCiclo = (id: number|string) => this.http.delete(`${this.API_URI}/materias/${id}`);
+  updateCiclo = (id:number|string, updatedMateria: Materia): Observable<Materia> => this.http.put(`${this.API_URI}/materias/${id}`,updatedMateria);
+
 }
