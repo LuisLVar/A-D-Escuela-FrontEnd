8234952bd96264cfb80ff247bf183602d93306b6
Feliciano07 <fernandoxhajon122@gmail.com>
2020-08-24 22:37:21 -0600

    Se agrego la direccion de las peticiones

--- src/app/services/ciclo_escolar/ciclo-escolar.service.ts
+++ src/app/services/ciclo_escolar/ciclo-escolar.service.ts
@@ -10,29 +10,29 @@ import { Observable } from 'rxjs';
 })
 export class CicloEscolarService {
 
-  //TODO cambiar la direccion por ip o dominio
-  API_URI = 'direccion';
+  
+  API_URI = 'http://3.20.3.133:3005/api';
 
   constructor(private http: HttpClient) { }
 
   getCiclos(){
-    return this.http.get(`${this.API_URI}/ciclos`);
+    return this.http.get(`${this.API_URI}/ciclo`);
   }
 
   getCiclo(id: string){
-    return this.http.get(`${this.API_URI}/ciclos/${id}`);
+    return this.http.get(`${this.API_URI}/ciclo/${id}`);
   }
 
   saveCiclo(ciclo: Ciclo){
-    return this.http.post(`${this.API_URI}/ciclos`,ciclo);
+    return this.http.post(`${this.API_URI}/ciclo`,ciclo);
   }
 
-  deleteCiclo(id: number){
-    return this.http.delete(`${this.API_URI}/ciclos/${id}`);
+  deleteCiclo(id: number | string){
+    return this.http.delete(`${this.API_URI}/ciclo/${id}`);
   }
 
   updateCiclo(id:number, updatedCiclo: Ciclo): Observable<Ciclo> {
-    return this.http.put(`${this.API_URI}/ciclos/${id}`,updatedCiclo);
+    return this.http.put(`${this.API_URI}/ciclo/${id}`,updatedCiclo);
   }
 
 }
