c0dcbe142e3926ec32af655e5f2d7797cc39061e
Elizabethz7 <7721Elizabethz7@users.noreply.github.com>
2020-08-24 21:24:18 -0600

    Servicios
    Servicios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { PortadaComponent } from './components/portada/portada.component';
+<<<<<<< Updated upstream
 import { LoginComponent } from './components/login/login.component';
+=======
+import { SeccionesComponent } from './components/secciones/secciones.component';
+>>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     PortadaComponent,
+<<<<<<< Updated upstream
     LoginComponent
+=======
+    SeccionesComponent
+>>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/secciones/secciones.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-secciones',
+  templateUrl: './secciones.component.html',
+  styleUrls: ['./secciones.component.css']
+})
+export class SeccionesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
