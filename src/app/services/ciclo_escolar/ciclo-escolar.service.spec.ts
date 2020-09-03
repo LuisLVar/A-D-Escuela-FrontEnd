48dfe3cd3338bf886a80cde1bbc64cbdfdb70555
Feliciano07 <fernandoxhajon122@gmail.com>
2020-09-03 14:43:44 -0600

    Prueba unitaria para la creacion de los servicios de ciclo escolar

--- src/app/services/ciclo_escolar/ciclo-escolar.service.spec.ts
+++ src/app/services/ciclo_escolar/ciclo-escolar.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CicloEscolarService } from './ciclo-escolar.service';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 
 describe('CicloEscolarService', () => {
   let service: CicloEscolarService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ]
+    });
     service = TestBed.inject(CicloEscolarService);
   });
 
