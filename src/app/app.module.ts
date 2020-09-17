93287369a4fa31d91f7120d514d23c77c39a6958
Gerardo cHAY <2995058550101@ingenieria.usac.edu.gt>
2020-09-16 22:09:13 -0600

    Creación de componenete colores

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { SeccionesComponent } from './components/secciones/secciones.component';
 import { GradoComponent } from './components/grados/grado/grado.component';
 import { AsigMateriaComponent } from './components/asig-materia/asig-materia.component';
 import { PersonalComponent } from './components/personal/personal.component';
+import { ColoresComponent } from './components/colores/colores.component';
 
 @NgModule({
   declarations: [
@@ -34,7 +35,8 @@ import { PersonalComponent } from './components/personal/personal.component';
     SeccionesComponent,
     GradoComponent,
     AsigMateriaComponent,
-    PersonalComponent
+    PersonalComponent,
+    ColoresComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/colores/colores.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-colores',
+  templateUrl: './colores.component.html',
+  styleUrls: ['./colores.component.css']
+})
+export class ColoresComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
