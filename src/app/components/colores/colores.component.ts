1bc6c45dc46e00f358946745f09ec35061dbec35
Gerardo cHAY <2995058550101@ingenieria.usac.edu.gt>
2020-09-23 22:46:05 -0600

    Se realizo el html de colores

--- src/app/components/colores/colores.component.ts
+++ src/app/components/colores/colores.component.ts
@@ -9,11 +9,16 @@ export class ColoresComponent implements OnInit {
 
   colorcito: any = '';
   descripcion: any = '';
-  colores: any[]=[];
+  colores: any[];
 
   constructor() { }
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.colores = [{
+      colorcito: '#832525',
+      descripcion: 'Color para lenguaje'
+    }];
+  }
   crearColor() { }
   confirmarColor() {}
-
+  eliminarColor(){}
 }
