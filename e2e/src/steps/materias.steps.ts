e52eaa33b685949feefb19b0f366c8ddde89abce
Victor López <2351772100101@ingenieria.usac.edu.gt>
2020-09-25 21:10:51 -0600

    Finalizando BDD de materias

---
+++ e2e/src/steps/materias.steps.ts
@@ -0,0 +1,27 @@
+import { Given, When, Then, Before } from 'cucumber';
+import { CrudMateria } from '../app.po';
+import {expect} from 'chai';
+
+let crudMateria: CrudMateria;
+
+Before (() =>{
+    crudMateria = new CrudMateria();
+})
+
+Given('Que Me encuentre en la pagina de materia', async function () {
+    await crudMateria.navigateTo();
+});
+
+When('presione el boton de nuevo_materia e ingrese un nueva materia', async function () {
+    await crudMateria.btnNuevo.click();
+    await crudMateria.setValuesCrear('Matematicas', 'Contenido');
+    expect(await crudMateria.nombre_materia.getAttribute('value')).to.equal('Matematicas');
+    expect(await crudMateria.contenido_materia.getAttribute('value')).to.equal('Contenido');
+});
+
+Then('Presionar el boton de guardar para que el valor se mande y se limpia', async function () {
+    await crudMateria.btnGuardar.click();
+    expect(await crudMateria.nombre_materia.getAttribute('value')).to.equal('Matematicas');
+    expect(await crudMateria.contenido_materia.getAttribute('value')).to.equal('Contenido');
+});
+});
