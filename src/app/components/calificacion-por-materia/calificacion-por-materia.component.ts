9a40fbeea6808dc69990b69f2b4af3ce3c31e300
Feliciano07 <fernandoxhajon122@gmail.com>
2020-10-22 14:50:16 -0600

    Se agrego la visualizacion de las materias y cambio en la tabla de ver los resultados obtenidos por la busquedad

--- src/app/components/calificacion-por-materia/calificacion-por-materia.component.ts
+++ src/app/components/calificacion-por-materia/calificacion-por-materia.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import {CalificacionMateriaService} from '../../services/calificacion/calificacion-materia.service';
+import {MateriaService} from '../../services/materias/materia.service';
 
 @Component({
   selector: 'app-calificacion-por-materia',
@@ -9,20 +10,38 @@ import {CalificacionMateriaService} from '../../services/calificacion/calificaci
 })
 export class CalificacionPorMateriaComponent implements OnInit {
 
-  public buscar: string;
+  public buscar: any = {
+    materia: '',
+    seccion: 0,
+    bloque: 0
+  };
+  public lista_materias: any = []
+
+  //TODO LA SECCION TAL VEZ SE CAMBIE CONFORME AL USUARIO QUE ESTA LOGUEADO
 
   public list_resultados: any = [];
 
-  constructor(private calificacionService: CalificacionMateriaService) { }
+  constructor(private calificacionService: CalificacionMateriaService, private materiaService: MateriaService) { }
 
   ngOnInit(): void {
+    this.obtener_Materias();
   }
 
   Validar_Campo(): boolean{
-    if(this.buscar === ''){
+    if(this.buscar.materia === '' && this.buscar.seccion !=0 ){
       return false;
     }
     return true;
+  }
+
+  obtener_Materias (){
+    this.materiaService.getMaterias().subscribe(
+      res => {
+        //console.log(this.listMateria)
+        this.lista_materias = res;
+      },
+      err => console.log(err)
+    );
   }
 
   Buscar_Materia(){
