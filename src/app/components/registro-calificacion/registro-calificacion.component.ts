00cb7f251c959e53e04a426cb5b5aa9e7072badf
Elizabethz7 <7721Elizabethz7@users.noreply.github.com>
2020-10-30 02:52:22 -0600

    Registro interfaz
    Registro interfaz

--- src/app/components/registro-calificacion/registro-calificacion.component.ts
+++ src/app/components/registro-calificacion/registro-calificacion.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AnyNaptrRecord } from 'dns';
+import { SeccionesService } from '../../services/secciones.service';
+import {CalificacionService} from '../../services/calificacion/calificacion.service'
+import {Alumno} from '../../models/alumnos';
+import {Materia} from '../../models/materia';
 @Component({
   selector: 'app-registro-calificacion',
   templateUrl: './registro-calificacion.component.html',
@@ -7,9 +11,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroCalificacionComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _seccion: SeccionesService, private _calificacion:CalificacionService) { }
+  personal:any;
+  seccion:any;
+  materias:any=[];
+  //quemados para mientras
+  alumnospeticiones:any=[];
+  alumnos=[];
+  alumno1:Alumno={}
+  calificacion={seccion:-1,alumno:-1,materia:-1,bloque:-1};
 
   ngOnInit(): void {
+    //por ahora quemado del login
+    //se tiene que personal es
+    this.personal = {personal:1,nombre:'Maritza'};
+    //get seccion del profesor
+    //this.getSeccion(this.personal.personal); 
+    //seccion para mientras
+    this.seccion={seccion:4};
+    console.log(new Date().getFullYear());
+    //materias
+    this.getMaterias(this.seccion.seccion);
+
   }
+  getSeccion(id:number): void {
+    this._seccion.getSeccionPersonal(id,'2020').subscribe(//TODO
+      res => {
+        console.log(res);
+        this.seccion=res;
+        
+      },
+      err => console.log(err)
+    );
+  }
+  getMaterias(id:number): void {
+    this._calificacion.getMateriasSecc(id).subscribe(
+      res => {
+        console.log(res);
+        this.materias=res;
+        
+      },
+      err => console.log(err)
+    );
+  }
+  change(){
+    if(this.calificacion.alumno!=-1 && this.calificacion.materia!=-1 && this.calificacion.bloque!=-1 && this.calificacion.seccion!=-1){
+      //hacer peticion
+    }
+  }
+
+
+
 
 }
---
+++ src/app/services/calificacion/calificacion.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Seccion } from '../models/Seccion';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CalificacionService {
+
+   //Variable para la dirección IP del host
+   API_URI = 'http://3.20.3.133:3010/api';
+
+  constructor(private http: HttpClient) { }
+
+  getMateriasSecc = (id: number) => this.http.get(`${this.API_URI}/seccion_materia/${id}`);
+  getAlumnos = (seccion: number,bloque:number) => this.http.get(`${this.API_URI}/inscripciones/alumnosBloque/${seccion}&${bloque}`);
+
+}
